d912f714f1b0d853144d8023df2ea65e026672ac
cleisommais <cleisommais@gmail.com>
2022-12-01 08:09:29 -0600

    Added array

---
+++ array.rb
@@ -0,0 +1,9 @@
+array = []
+puts "Array elements #{array}"
+array.push(10)
+array.push(20)
+array.push(30)
+array.push(44)
+puts "Array elements #{array}"
+array.delete(30)
+puts "Array elements #{array.length}"
