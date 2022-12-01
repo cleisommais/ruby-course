bc4c3c2cecbe061ecc83ed50cc869cb99544a649
cleisommais <cleisommais@gmail.com>
2022-12-01 08:14:54 -0600

    added hashes

---
+++ hashes.rb
@@ -0,0 +1,8 @@
+#hashes = map 
+hashes = {'a'=>'b', 'c'=>'d'}
+puts "hashes = #{hashes}"
+puts "element = #{hashes['c']}"
+
+hashes2 = {a:'b', c:'d'}
+puts "hashes2 = #{hashes2}"
+puts "element = #{hashes2[:c]}"
