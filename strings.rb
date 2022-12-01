2967558964aa03e1cd88ce447f16e3ed0b8d7a6b
cleisommais <cleisommais@gmail.com>
2022-11-30 19:44:35 -0600

    strings

---
+++ strings.rb
@@ -0,0 +1,9 @@
+#
+name = 'concat'
+puts "xpto " + name
+# it does not work, to interpolation you need to use double quotes
+puts 'xpto2 #{name}'
+# interpolation example
+puts "xpto3 #{name}"
+
+puts "Sum 2 + 3 = #{2 + 3}"
