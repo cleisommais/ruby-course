b572226725009de42c04ef264a3b64a51f8ea49e
cleisommais <cleisommais@gmail.com>
2022-12-01 08:02:33 -0600

    added repetition structure

---
+++ rep_structure.rb
@@ -0,0 +1,12 @@
+#while is true
+count = 0
+while count <= 10
+    puts "Count is #{count}"
+    count += 1
+end
+
+#until is false
+until count <= 0
+    puts "Count is #{count}"
+    count -= 1    
+end
