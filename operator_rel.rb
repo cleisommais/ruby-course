aac86fc6c98bac73496a3dd5d09f5fc5e8370128
cleisommais <cleisommais@gmail.com>
2022-11-30 20:00:52 -0600

    added oper rela

---
+++ operator_rel.rb
@@ -0,0 +1,16 @@
+#more than
+puts 10 > 5
+#more than and equals to
+puts 10 >= 5
+# less than
+puts 10 < 5
+#less than and equals to
+puts 10 >= 5
+#equals to
+puts 10 == 5
+#no equals to
+puts 10 != 5
+#check all situations
+puts 10 <=> 5
+puts 10 <=> 10
+puts 5 <=> 10
