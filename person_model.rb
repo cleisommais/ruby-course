5a14314a0655247027f06fd6aa1c57dc58610088
cleisommais <cleisommais@gmail.com>
2022-12-02 09:02:42 -0600

    Added person class

---
+++ person_model.rb
@@ -0,0 +1,15 @@
+class Person
+    #method should use snake_case convension
+    def speak
+        puts "I'm speaking!"
+    end
+    def walk
+        puts "I'm walking!"
+    end
+end
+
+person = Person.new 
+
+person.speak
+
+person.walk
