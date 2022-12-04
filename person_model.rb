4cf3c620160d2fb4bc37d64e21a3d2acc1fdc1c2
cleisommais <cleisommais@gmail.com>
2022-12-04 16:52:42 -0600

    added attributes and changed the methods

--- person_model.rb
+++ person_model.rb
@@ -1,17 +1,28 @@
 class Person
+    #properties should use snake_case conversion
+    @name = nil
+    @age = nil
     #method should use snake_case convension
-    def speak(text = "I'm speaking")
-        return text
+    def name=(name)
+        @name = name 
+    end 
+    def name
+        @name
     end
-    def walk
-        puts "I'm walking!"
+    def age=(age)
+        @age = age 
+    end
+    def age
+        @age
     end
 end
 
 person = Person.new 
 
-puts person.speak
+person.name = 'Jose Maria'
+person.age = 33
+puts person.name
+puts person.age
 
-puts person.speak("xpto")
 
-person.walk
+
