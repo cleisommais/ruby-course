dd498475a2178161f7c9209c9a5770a0069244f5
cleisommais <cleisommais@gmail.com>
2022-12-05 08:04:45 -0600

    Guess number class

---
+++ guess_number_model.rb
@@ -0,0 +1,30 @@
+class GuessNumber
+    attr_reader :number
+    attr_reader :winner    
+    def initialize
+        range = Random.new
+        #get a range of numbers between 0 and 10
+        @number = range.rand(0..10)
+        @winner = false
+    end
+
+    def guess(number)
+        if number == @number 
+            @winner = true
+            "You won!"
+        elsif number > @number
+            "Number is too high..."
+        else 
+            "Number is too low..."
+        end
+    end
+end
+
+game = GuessNumber.new
+#until winner is false continue
+until game.winner
+    puts "Guess the number"
+    number = gets.to_i
+    puts game.guess(number)    
+end
+
