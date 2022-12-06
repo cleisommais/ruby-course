6e439446ca2364f289cb90385031f3acff848cfd
cleisommais <cleisommais@gmail.com>
2022-12-06 08:29:49 -0600

    argument

---
+++ argv.rb
@@ -0,0 +1,12 @@
+# ruby argv.rb file.txt
+puts ARGV
+
+if ARGV.size.positive?
+    File.open(ARGV[0], "r") do |file|
+        while lines=file.gets
+            puts lines
+        end
+    end   
+else 
+    puts "Missing file name as argument" 
+end
