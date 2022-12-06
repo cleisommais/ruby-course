8abf481a4ea6425de25847eb193cdbda064c1b17
cleisommais <cleisommais@gmail.com>
2022-12-05 20:40:44 -0600

    sample constant

---
+++ constanst.rb
@@ -0,0 +1,13 @@
+#constant is upper case
+NUMBER = 123
+puts NUMBER 
+
+#You cant assign a new value to NUMBER
+#NUMBER = 1
+
+class TestConstant
+    PI = 3.14
+end
+
+#one constant you call it like a static method
+puts TestConstant::PI
